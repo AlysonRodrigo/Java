c2be16b3a1ac61b6f30828f311059d2f0ef8e9b7
unknown <alysonrodrigo14@hotmail.com>
2021-08-12 00:18:56 -0300

    inicio da lista

---
+++ Lista/listas.java
@@ -0,0 +1,22 @@
+package Turma29;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+public class listas {
+
+	public static void main(String[] args) {
+		ArrayList <String> loja =new ArrayList<String>();
+		loja.add("Abacaxi");
+		loja.add("maçã");
+		loja.add("Banana");
+		loja.add("Pera");
+
+		
+		
+	}
+}
